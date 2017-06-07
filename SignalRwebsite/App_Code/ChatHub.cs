b42fed7b44099026260ac4c8b404fa7b8ddf5c23
Dimonina <dimonina@gmail.com>
2017-06-07 11:36:41 +0800

    redundant usings removed

--- SignalRwebsite/App_Code/ChatHub.cs
+++ SignalRwebsite/App_Code/ChatHub.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Web;
 using Microsoft.AspNet.SignalR;
 
 namespace SignalRwebsite.App_Code
